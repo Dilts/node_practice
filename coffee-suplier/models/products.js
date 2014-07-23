8203743d1f7116e0ebb09981e23e563e8b943697
Brian Dilts <brianedilts@gmail.com>
2014-07-23 10:39:49 -0600

    Refactoring Node with MVC

---
+++ coffee-suplier/models/products.js
@@ -0,0 +1,24 @@
+var products = [
+	{
+		name: 'Dark Roast',
+		price: 1.92,
+		origin: 'Brazil'
+	},	
+	{
+		name: 'Columbian',
+		price: 1.50,
+		origin: 'Columbia'
+	},	
+	{
+		name: 'Kenyan',
+		price: 1.75,
+		origin: 'Kenya'
+	},	
+	{
+		name: 'French Roast',
+		price: 2.12,
+		origin: 'Etiopia'
+	}
+]
+
+module.exports = products;
